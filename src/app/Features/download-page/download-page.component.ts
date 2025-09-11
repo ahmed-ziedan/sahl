af656501d0841cdf81ff4d4fec554613c75a52e3
ahmed <az744257@gmail.com>
2025-09-11 18:56:59 +0300

    downloadPage

---
+++ src/app/Features/download-page/download-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-download-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './download-page.component.html',
+  styleUrl: './download-page.component.scss'
+})
+export class DownloadPageComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './Features/contact/contact.component';
+import { AboutUsComponent } from './Features/about-us/about-us.component';
+import { DownloadPageComponent } from './Features/download-page/download-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'download-app', component: DownloadPageComponent },
 ];
