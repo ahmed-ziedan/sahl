64d51db9195b0297ff956023cb9412f987614e9b
ahmed <az744257@gmail.com>
2025-09-13 01:19:09 +0300

    sahl services content

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
-
+import {
+  provideRouter,
+  withHashLocation,
+  withInMemoryScrolling,
+} from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation()), provideHttpClient()],
+  providers: [
+    provideRouter(
+      routes,
+      withHashLocation(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ),
+    provideHttpClient(),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,43 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './Features/contact/contact.component';
 import { AboutUsComponent } from './Features/about-us/about-us.component';
 import { DownloadPageComponent } from './Features/download-page/download-page.component';
+import { ServicesComponent } from './Features/services/services.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'about', component: AboutUsComponent },
-  { path: 'download-app', component: DownloadPageComponent },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then((c) => c.HomeComponent),
+  },
+
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./Features/contact/contact.component').then(
+        (c) => c.ContactComponent
+      ),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./Features/about-us/about-us.component').then(
+        (c) => c.AboutUsComponent
+      ),
+  },
+
+  {
+    path: 'download-app',
+    loadComponent: () =>
+      import('./Features/download-page/download-page.component').then(
+        (c) => c.DownloadPageComponent
+      ),
+  },
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./Features/services/services.component').then(
+        (c) => c.ServicesComponent
+      ),
+  },
 ];
